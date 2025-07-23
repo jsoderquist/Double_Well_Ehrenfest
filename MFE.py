941970739c8b9afe8fc2a034d89083c43a776863
jsoderquist <93695473+jsoderquist@users.noreply.github.com>
2025-07-23 12:18:41 -0400

    Update MFE.py

--- MFE.py
+++ MFE.py
@@ -5,26 +5,26 @@ import parameters as par
 # ==================================
 
 # FORCE 1 UPDATE FUNCTION
-@nb.jit(nopython=True, fastmath=True)
+# @nb.jit(nopython=True, fastmath=True)
 def Force1(data):
     data.F1[:]    = 0
-    data.F1[:]   -= par.ωj[:]**2 * data.x[:] 
-    par_sum  = np.sum(par.dHij * data.ρt * 1.0, axis= 1).real    # NUMBA DOES NOT ALLOW THE SUMMATION OVER TWO AXIS, MUST BE DONE IN TWO STEPS
+    data.F1[:]   -= data.ωj[:]**2 * data.x[:] 
+    par_sum  = np.sum(par.dHij_cons(data) * data.ρt * 1.0, axis= 1).real    # NUMBA DOES NOT ALLOW THE SUMMATION OVER TWO AXIS, MUST BE DONE IN TWO STEPS
     data.F1[:]   -= np.sum(par_sum, axis = 1).real
 
 # FORCE 2 UPDATE FUNCTION
-@nb.jit(nopython=True, fastmath=True)
+# @nb.jit(nopython=True, fastmath=True)
 def Force2(data):
     data.F2[:]    = 0
-    data.F2[:]   -= par.ωj[:]**2 * data.x[:] 
-    par_sum = np.sum(par.dHij * data.ρt * 1.0, axis= 1).real    # NUMBA DOES NOT ALLOW THE SUMMATION OVER TWO AXIS, MUST BE DONE IN TWO STEPS
+    data.F2[:]   -= data.ωj[:]**2 * data.x[:] 
+    par_sum = np.sum(par.dHij_cons(data) * data.ρt * 1.0, axis= 1).real    # NUMBA DOES NOT ALLOW THE SUMMATION OVER TWO AXIS, MUST BE DONE IN TWO STEPS
     data.F2[:]   -= np.sum(par_sum, axis = 1).real
 
 # RUNGE - KUTTA PROPAGATOR
 # THIS DOES HALF OF THE ELETRONIC STEPS, par.Estep/2 | Estep MUST BE EVEN!!!
 @nb.jit(nopython=True, fastmath=True)
 def RK4(data):
-    H  = par.Hel * 1.0 + data.H_bc * 1.0
+    H  = par.Hel_cons(data) * 1.0 + data.H_bc * 1.0
     ρ  = data.ρt * 1.0
     dt = par.dtE * 1.0
     for k in range(int(par.Estep/2)):
@@ -41,7 +41,7 @@ def von_Newman(ρf, H):
     return -1j * (H @ ρf - ρf @ H)
 
 #  VELOCITY VERLET PROPAGATOR
-@nb.jit(nopython=True, fastmath=True)
+# @nb.jit(nopython=True, fastmath=True)
 def VelVer(data) : 
     data.v[:] = data.P[:]/par.M * 1.0                                           # VELOCITY           
     RK4(data)                                                                   # ELECTRONIC UPDATE | RK4 DOES HALF OF THE ELECTRONIC PROPAGATION!!!!
@@ -55,10 +55,11 @@ def VelVer(data) :
     # ======================================================
 
 # RUN TRAJECTORIES
-@nb.jit(nopython=True, fastmath=True)
+# @nb.jit(nopython=True, fastmath=True)
 def run_traj(data):
     model.initR(data)                           # INITIALIZE x AND P FOR ALL BATH MODES
     data.ρt = par.ρ0                            # INITIAL DENSITY MATRTIX
+    # ωj = par.calc_ωj(ωc)                            # calculate ωj for this set of ωc
     Force1(data)                                # FORCE 1 AT t = 0
     model.H_BC(data)                            # INITIAL ELECTRONIC HAMILTONIAN UPDATE | BATH POSITION DEPEDENT PART 
 
